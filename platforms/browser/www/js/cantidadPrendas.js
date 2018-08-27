3b8a933f81add757ce1a524a325474576f54ab03
Jonathan Gonzalez <jg422930@gmail.com>
2018-08-27 15:11:47 -0300

    Cambios en el formulario de corte

---
+++ platforms/browser/www/js/cantidadPrendas.js
@@ -0,0 +1,26 @@
+var nombre;
+var nroCorte;
+var fecha = new Date();
+var talle;
+var taco;
+var pares;
+var color;
+
+
+
+nombre = "Pepe";
+nroCorte = 2;
+talle = [1,2,3,4,5,6]
+taco = 2
+pares = 21.5
+color = ["Blanco"]
+prendas = taco*pares
+
+console.log(nombre);
+console.log(nroCorte);
+console.log(fecha);
+console.log(talle.toString());
+console.log(taco);
+console.log(pares);
+console.log(color.toString());
+console.log(prendas)
