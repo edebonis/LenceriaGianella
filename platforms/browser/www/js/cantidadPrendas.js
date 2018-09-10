2a12a4a95ab082b7705d355fd3e0a01536150bea
Evelyn Torres <evelyn.torres@sagradoalcal.edu.ar>
2018-09-10 15:11:28 -0300

    Solucion de error con onclick

--- platforms/browser/www/js/cantidadPrendas.js
+++ platforms/browser/www/js/cantidadPrendas.js
@@ -1,3 +1,39 @@
+document.getElementById("botonEnviar").addEventListener("click", myFunction);
+
+function myFunction(){
+    
+    var nombre = document.getElementById("Nombre").value;
+    var nroCorte = document.getElementById("nroCorte").value;
+
+    var fecha = new Date();
+    
+    var talle = [1,2,3,4,5,6];
+	var taco = 2;
+	var pares = 21.5;
+	var prendas = taco*pares;
+
+	if(document.getElementById("Rojo").checked){
+		color = "Rojo";	
+	}
+
+	if(document.getElementById("Negro").checked){
+		color = "Negro";	
+	}
+
+	if(document.getElementById("Blanco").checked){
+		color = "Blanco";	
+	}
+
+	console.log(nombre);
+	console.log(nroCorte);
+	console.log(fecha);
+	console.log(talle);
+	console.log(taco);
+	console.log(pares);
+	console.log(color);
+	console.log(prendas);
+}
+/*
 var nombre;
 var nroCorte;
 var fecha = new Date();
@@ -24,10 +60,7 @@ if(document.getElementById("Blanco").checked){
 color = "Blanco";	
 }
 
-talle = [1,2,3,4,5,6];
-taco = 2;
-pares = 21.5;
-prendas = taco*pares;
+
 
 console.log(nombre);
 console.log(nroCorte);
@@ -35,6 +68,9 @@ console.log(fecha);
 console.log(talle.toString());
 console.log(taco);
 console.log(pares);
-console.log(color.toString());
+//console.log(color.toString());
 console.log(prendas);
 }
+
+document.getElementById("botonEnviar").addEventListener("click", enviar);
+*/
