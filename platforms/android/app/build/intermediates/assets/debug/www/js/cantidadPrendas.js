9851c3a79e362889f4e18ad73b6d7f40f013e634
Rodrigo Márquez <rmarquez@sagradoalcal.edu.ar>
2018-10-31 14:56:31 -0300

    agregué plataforma android

---
+++ platforms/android/app/build/intermediates/assets/debug/www/js/cantidadPrendas.js
@@ -0,0 +1,79 @@
+document.getElementById("botonEnviar").addEventListener("click", myFunction);
+
+function myFunction(){
+    
+    var nombre = document.getElementById("Nombre").value;
+    var nroCorte = document.getElementById("nroCorte").value;
+
+    var fecha = new Date();
+    
+    var talle = [1,2,3,4,5,6];
+	var taco = 2;
+	var pares = 21.5;
+	var prendas = taco*pares;
+
+	if(document.getElementById("Rojo").checked){
+		color = "Rojo";	
+	}
+
+	if(document.getElementById("Negro").checked){
+		color = "Negro";	
+	}
+
+	if(document.getElementById("Blanco").checked){
+		color = "Blanco";	
+	}
+
+
+	window.localStorage.setItem('color',color);
+
+	console.log(nombre);
+	console.log(nroCorte);
+	console.log(fecha);
+	console.log(talle);
+	console.log(taco);
+	console.log(pares);
+	console.log(window.localStorage.getItem('color'));
+	console.log(prendas);
+}
+/*
+var nombre;
+var nroCorte;
+var fecha = new Date();
+var talle;
+var taco;
+var pares;
+var color;
+
+
+function enviar(){
+nombre = document.getElementById("Nombre").value;
+nroCorte = document.getElementById("nroCorte").value;
+
+
+if(document.getElementById("Rojo").checked){
+color = "Rojo";	
+}
+
+if(document.getElementById("Negro").checked){
+color = "Negro";	
+}
+
+if(document.getElementById("Blanco").checked){
+color = "Blanco";	
+}
+
+
+
+console.log(nombre);
+console.log(nroCorte);
+console.log(fecha);
+console.log(talle.toString());
+console.log(taco);
+console.log(pares);
+//console.log(color.toString());
+console.log(prendas);
+}
+
+document.getElementById("botonEnviar").addEventListener("click", enviar);
+*/
